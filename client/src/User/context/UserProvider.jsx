79cad98c84a1ba3452324621489e03eecf855661
uniquetraker <uniquetraker@gmail.com>
2024-10-01 12:05:22 +0530

    change

--- client/src/User/context/UserProvider.jsx
+++ client/src/User/context/UserProvider.jsx
@@ -9,7 +9,7 @@ export const UserProvider = createContext();
 
 export default function UserContext({ children }) {
   const dispatch = useDispatch();
-  // const { userData } = useSelector((state) => state.UserSliceProvider);
+  const { userData } = useSelector((state) => state.UserSliceProvider);
   useEffect(() => {
     dispatch(getAllUsersFetchApi());
   }, []);
@@ -21,11 +21,11 @@ export default function UserContext({ children }) {
 
 
   useEffect(() => {
-  // dispatch(getCartByUserId(userData._id))
+  dispatch(getCartByUserId(userData._id))
 },[dispatch])
   // console.log(userData);
 
   return (
-    <UserProvider.Provider value={""}>{children}</UserProvider.Provider>
+    <UserProvider.Provider value={userData}>{children}</UserProvider.Provider>
   );
 }
